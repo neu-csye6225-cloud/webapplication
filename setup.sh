24f43982643ff86323f4d96232f83a9f393bd27e
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-10-28 16:33:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,135 +1,60 @@
 #!/bin/bash
 
- 
-
 
  
 
+ 
 sudo apt-get update
-
- 
-
- 
-
- 
-
-# Install Node.js and npm
-
- 
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
-
-
-# Install MariaDB server and client
-
- 
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server mariadb-client
-
- 
-
- 
-
- 
-
 # Check Node.js version
 
  
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
 
 sudo DEBIAN_FRONTEND=noninteractive node -v
-
- 
-
- 
-
- 
-
-# Check npm version
-
- 
-
-npm -v
-
- 
-
- 
-
- 
-
-# Install unzip
-
  
 
 sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
 
- 
-
- 
-
- 
 
 # Unzip the WebAppRenamed file to the WebApp directory
 
- 
-
 sudo unzip WebAppRenamed -d WebApp
-
- 
-
- 
-
- 
 
 # Start MySQL shell
 
- 
-
 sudo mysql
 
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
 
 sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES; CREATE DATABASE Assignment3;"
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
 # Exit MySQL shell
 
- 
 
 sudo mysql --execute="EXIT;"
 
- 
-
- 
-
- 
-
 # Display a message
 
- 
 
-echo "foo"
+echo "web application service starting now"
+# webapplication system service
+# Install and configure your service
+
+
+sudo cp /home/admin/WebApp/webapplication.service /lib/systemd/system/webapplication.service
+
+
+sudo cp /home/admin/WebApp/webapplication.service /lib/systemd/system/webapplication.service
+echo "did we copy the files"
+
+
+echo "started the web application"
+
+
+sudo systemctl start webapplication.service
+echo "enabled the web application"
+sudo systemctl enable webapplication.service
+
+
+sudo systemctl daemon-reload
+
+
+# webapplication system service
