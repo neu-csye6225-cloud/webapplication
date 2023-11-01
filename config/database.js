bc579e282912d685751a4862627d0286b8e1c323
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-11-01 02:41:21 -0400

    Update database.js (#35)

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config()
 const sequelize = new Sequelize({
 
-  dialect: "mysql",
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   
 });
