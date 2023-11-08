61291ca04009d95aa7dc85020a7dd01781aebb79
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-11-08 07:36:23 -0500

    Assignment7 (#39)
    * added setup files
    
    * setup files added cloudwatch
    
    * Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,13 +20,21 @@ sudo systemctl start amazon-cloudwatch-agent
 # Verify the status of the agent
 sudo systemctl status amazon-cloudwatch-agent
 
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
 
 sudo DEBIAN_FRONTEND=noninteractive node -v
  
 sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
 
+wget https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb.sig
+
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m on
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m status
 sudo unzip WebAppRenamed -d WebApp
 
 sudo groupadd csye6225
