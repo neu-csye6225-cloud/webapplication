be4b92721e35360f7b3e9e0b21a10aa7e4363be7
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-11-08 07:47:54 -0500

    Update setup.sh (#40)

--- setup.sh
+++ setup.sh
@@ -2,23 +2,6 @@
 
 sudo apt-get update
 
-echo "installing cloudwatch"
-# Update the package manager
-sudo yum update -y
-
-# Install the CloudWatch Agent
-sudo yum install -y amazon-cloudwatch-agent
-
-# Start the CloudWatch Agent Configuration Wizard
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
-
-sudo systemctl enable amazon-cloudwatch-agent
-
-# Start the CloudWatch Agent service
-sudo systemctl start amazon-cloudwatch-agent
-
-# Verify the status of the agent
-sudo systemctl status amazon-cloudwatch-agent
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
 
@@ -26,6 +9,8 @@ sudo DEBIAN_FRONTEND=noninteractive node -v
  
 sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
 
+
+echo "installing cloudwatch"
 wget https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb.sig
 
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
