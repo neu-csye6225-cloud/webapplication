332d5ea44de15011f5d85d64ea003c459d13f93c
Sheetal Pujari <pujari.sh@northeastern.edu>
2023-11-08 13:10:35 -0500

    added setup

--- setup.sh
+++ setup.sh
@@ -16,8 +16,63 @@ sudo apt-get install rpm
 wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
 
 sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo apt-get -f install -y
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+# Create the CloudWatch agent configuration file
+sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json > /dev/null <<EOF
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "cwagent"
+  },
+  "metrics": {
+    "append_dimensions": {
+      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}",
+      "ImageId": "\${aws:ImageId}",
+      "InstanceId": "\${aws:InstanceId}",
+      "InstanceType": "\${aws:InstanceType}"
+    },
+    "metrics_collected": {
+      "cpu": {
+        "measurement": ["usage_idle", "usage_user", "usage_system"],
+        "metrics_collection_interval": 60,
+        "totalcpu": false
+      },
+      "disk": {
+        "measurement": ["used_percent", "inodes_free"],
+        "metrics_collection_interval": 60,
+        "resources": ["/", "/var/nfs"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      }
+    }
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/messages",
+            "log_group_name": "sample-log-group",
+            "log_stream_name": "messages"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "cloudwatch-agent",
+    "force_flush_interval" : 15
+  },
+  "use_gzip_http_content_encoding": true
+}
+EOF
+
+# Start the CloudWatch agent and enable it on boot
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a configure -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
+sudo systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
+
 sudo unzip WebAppRenamed -d WebApp
 
 sudo groupadd csye6225
