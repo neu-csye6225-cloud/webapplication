b111de1e975f14d48cc8f603f4c9f2c698b0c1a8
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-11-08 09:07:15 -0500

    Assign7 (#43)
    * updated sh files
    
    * added cloudwatch config

--- setup.sh
+++ setup.sh
@@ -11,17 +11,43 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
 
 
 echo "installing cloudwatch"
-wget https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb.sig
 
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo apt install amazon-cloudwatch-agent -y
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo tee /opt/aws/amazon-cloudwatch-agent/bin/config.json <<EOF 
+{
+  "agent": {
+      "metrics_collection_interval": 10,
+      "logfile": "/var/logs/amazon-cloudwatch-agent.log"
+  },
+  "logs": {
+      "logs_collected": {
+          "files": {
+              "collect_list": [
+                  {
+                      "file_path": "/var/log/tomcat9/csye6225.log",
+                      "log_group_name": "csye6225",
+                      "log_stream_name": "webapp"
+                  }
+              ]
+          }
+      },
+      "log_stream_name": "cloudwatch_log_stream"
+  },
+  "metrics":{
+    "metrics_collected":{
+       "statsd":{
+          "service_address":":8125",
+          "metrics_collection_interval":15,
+          "metrics_aggregation_interval":300
+       }
+    }
+ }
+}
+EOF
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
 
-echo "installing agent"
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m on
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m status
-
+tail -f /opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log
 
 sudo unzip WebAppRenamed -d WebApp
 
