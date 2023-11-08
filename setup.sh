e050219fd1810eb6c3e11c28e7bb0662d324a371
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-11-08 07:01:20 -0500

    Assignment7 (#38)
    * added setup files
    
    * setup files added cloudwatch

--- setup.sh
+++ setup.sh
@@ -3,74 +3,23 @@
 sudo apt-get update
 
 echo "installing cloudwatch"
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
+# Update the package manager
+sudo yum update -y
 
-echo "updating configuration of cloud watch agent"
-cat << EOF > /opt/aws/amazon-cloudwatch-agent/etc/cwagentconfig.json
-{
-  "agent": {
-    "metrics_collection_interval": 60,
-    "run_as_user": "cwagent"
-  },
-  "metrics": {
-    "append_dimensions": {
-      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}",
-      "ImageId": "\${aws:ImageId}",
-      "InstanceId": "\${aws:InstanceId}",
-      "InstanceType": "\${aws:InstanceType}"
-    },
-    "metrics_collected": {
-      "cpu": {
-        "measurement": [
-          "usage_idle",
-          "usage_iowait",
-          "usage_user",
-          "usage_system"
-        ],
-        "metrics_collection_interval": 60,
-        "resources": [
-          "*"
-        ]
-      },
-      "disk": {
-        "measurement": [
-          "used_percent",
-          "inodes_free"
-        ],
-        "metrics_collection_interval": 60,
-        "resources": [
-          "/"
-        ]
-      },
-      "mem": {
-        "measurement": [
-          "mem_used_percent"
-        ],
-        "metrics_collection_interval": 60
-      }
-    }
-  },
-  "logs": {
-    "logs_collected": {
-      "files": {
-        "collect_list": [
-          {
-            "file_path": "/var/log/messages",
-            "log_group_name": "var_log_messages",
-            "log_stream_name": "{instance_id}"
-          }
-        ]
-      }
-    },
-    "log_stream_name": "log_stream_name",
-    "force_flush_interval" : 15
-  }
-}
+# Install the CloudWatch Agent
+sudo yum install -y amazon-cloudwatch-agent
 
-# Start the CloudWatch Agent
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -c file:/opt/aws/amazon-cloudwatch-agent/etc/cwagentconfig.json -s
-In this script,
+# Start the CloudWatch Agent Configuration Wizard
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+sudo systemctl enable amazon-cloudwatch-agent
+
+# Start the CloudWatch Agent service
+sudo systemctl start amazon-cloudwatch-agent
+
+# Verify the status of the agent
+sudo systemctl status amazon-cloudwatch-agent
+
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
 
