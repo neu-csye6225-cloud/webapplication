d82247f9f6c7ac1d08cd8f63ed1d902d377934ee
Sheetal Pujari <pujari.sh@northeastern.edu>
2023-11-15 01:43:57 -0500

    added file fir json

--- setup.sh
+++ setup.sh
@@ -19,42 +19,7 @@ sudo dpkg -i amazon-cloudwatch-agent.deb
 sudo apt-get -f install -y
 
 # Create the CloudWatch agent configuration file
-sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json > /dev/null <<EOF
-{
-  "agent": {
-      "metrics_collection_interval": 10,
-      "logfile": "/var/logs/amazon-cloudwatch-agent.log"
-  },
-  "logs": {
-      "logs_collected": {
-          "files": {
-              "collect_list": [
-                  {
-                      "file_path": "/home/admin/WebApp/log/app.log",
-                      "log_group_name": "csye6225",
-                      "log_stream_name": "webapp"
-                  }
-              ]
-          }
-      },
-      "log_stream_name": "cloudwatch_log_stream"
-  },
-  "metrics":{
-    "metrics_collected":{
-       "statsd":{
-          "service_address":":8125",
-          "metrics_collection_interval":15,
-          "metrics_aggregation_interval":300
-       }
-    }
- }
-}
-EOF
-
-# Start the CloudWatch agent and enable it on boot
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a configure -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
-sudo systemctl enable amazon-cloudwatch-agent
-sudo systemctl start amazon-cloudwatch-agent
+sudo cp /home/admin/WebApp/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 sudo unzip WebAppRenamed -d WebApp
 
