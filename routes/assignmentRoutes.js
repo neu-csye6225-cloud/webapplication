4cbc0cd6e232a502d10f79c5827e7437015ae302
Sheetal <122663660+Sheetalpujarii@users.noreply.github.com>
2023-12-08 01:12:54 -0500

    Update assignmentRoutes.js

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -4,11 +4,11 @@ import { checkToken } from '../services/auth.js';
 
 const router = Router();
 
-router.get('/v1/assignments', checkToken, assignmentController.getAllAssignments);
-router.post('/v1/assignments', checkToken, assignmentController.createAssignment);
-router.get('/v1/assignments/:id', checkToken, assignmentController.getAssignmentById);
-router.delete('/v1/assignments/:id',checkToken,assignmentController.deleteAssignment);
-router.put('/v1/assignments/:id', checkToken, assignmentController.updateAssignment);
-router.post('/v1/assignments/:id/submission',checkToken, assignmentController.createSubmission);
+router.get('/v3/assignments', checkToken, assignmentController.getAllAssignments);
+router.post('/v3/assignments', checkToken, assignmentController.createAssignment);
+router.get('/v3/assignments/:id', checkToken, assignmentController.getAssignmentById);
+router.delete('/v3/assignments/:id',checkToken,assignmentController.deleteAssignment);
+router.put('/v3/assignments/:id', checkToken, assignmentController.updateAssignment);
+router.post('/v3/assignments/:id/submission',checkToken, assignmentController.createSubmission);
 
 export default router;
